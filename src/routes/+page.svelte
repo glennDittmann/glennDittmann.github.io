c45c6a93fd538670140895bc55a62c39d7a28568
glennDittmann <glenn.dittmann@posteo.de>
2024-05-17 11:07:28 +0200

    Add basic page layout that uses the three scene

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import { onMount } from 'svelte';
+    import { createScene } from '$lib/scene'
+
+    let el:HTMLCanvasElement;
+
+    onMount(() => {
+        createScene(el);
+    });
+</script>
+
+<svelte:head>
+    <title>My Homepage</title>
+    <meta name="description" content="Personal website built with Svelte and Three.js">
+</svelte:head>
+
+<canvas bind:this={el} />
