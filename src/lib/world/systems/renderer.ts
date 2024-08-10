9f7bfe399d1bda9c99d34cc8a74c157fb71e6fa2
glennDittmann <glenn.dittmann@posteo.de>
2024-08-10 09:39:28 +0200

    Rm old files for new setup

--- src/lib/world/systems/renderer.ts
+++
@@ -1,8 +0,0 @@
-import { WebGLRenderer } from 'three';
-
-export function createRenderer() {
-	// TODO find out if physicallyCorectLights is activate now as default, cause it is not accessible via the renderer anymore.
-	const renderer = new WebGLRenderer({antialias: true});
-
-	return renderer;
-}
