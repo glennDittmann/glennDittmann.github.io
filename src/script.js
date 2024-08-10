15c181b709f033c47c18ac954082817b670ad085
glennDittmann <glenn.dittmann@posteo.de>
2024-08-10 13:38:07 +0200

    06 -cameras

--- src/script.js
+++ src/script.js
@@ -1,5 +1,6 @@
 import './styles.css';
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import gsap from 'gsap';
 
 const sizes = {
@@ -15,8 +16,11 @@ const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0x91a889 });
 const mesh = new THREE.Mesh(geometry, material);
 
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
+// camera.position.x = 2;
+// camera.position.y = 2;
 camera.position.z = 3;
+camera.lookAt(mesh.position);
 
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
@@ -25,10 +29,15 @@ renderer.setSize(sizes.width, sizes.height);
 
 scene.add(camera, mesh);
 
+/** Controls */
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
 /** Animated render */
-gsap.to(mesh.position, { duration: 1, delay: 1, x: 2 });
+// gsap.to(mesh.position, { duration: 1, delay: 1, x: 2 });
 
 const tick = () => {
+    controls.update();
 
     renderer.render(scene, camera);
 
