ede6eb097b77b61c3c6c27ccc45f49cc699a9ad9
glennDittmann <glenn.dittmann@posteo.de>
2024-08-10 15:15:05 +0200

    08 - geometries

--- src/script.js
+++ src/script.js
@@ -41,9 +41,18 @@ const canvas = document.querySelector('canvas.webgl');
 
 const scene = new THREE.Scene();
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x91a889 });
+const geometry = new THREE.BoxGeometry(1, 1, 1, 2, 2, 2);
+const material = new THREE.MeshBasicMaterial({ color: 0x91a889, wireframe: true });
 const mesh = new THREE.Mesh(geometry, material);
+
+const bufferGeometry = new THREE.BufferGeometry();
+const posArray = new Float32Array(2 * 3 * 3);
+for (let i = 0; i < posArray.length; i++) {
+    posArray[i] = (Math.random() - 0.5) * 4;
+}
+const posAttr = new THREE.BufferAttribute(posArray, 3);
+bufferGeometry.setAttribute('position', posAttr);
+const mesh2 = new THREE.Mesh(bufferGeometry, material);
 
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
 // camera.position.x = 2;
@@ -57,7 +66,7 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
-scene.add(camera, mesh);
+scene.add(camera, mesh, mesh2);
 
 /** Controls */
 const controls = new OrbitControls(camera, canvas);
